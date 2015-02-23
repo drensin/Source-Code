77dde27ba06d792312aa620275de58a8eb357835
drensin <dave@rensin.com>
2015-02-23 00:12:35 -0800

    added user auth to sample

--- go/src/github.com/drensin/FirstAppEngineApp/hello.go
+++ go/src/github.com/drensin/FirstAppEngineApp/hello.go
@@ -1,20 +1,23 @@
 package hello
 
 import (
-	"fmt"
-	"net/http"
-
 	"appengine"
 	"appengine/user"
+	"fmt"
+	"html/template"
+	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", root)
+	http.HandleFunc("/sign", sign)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func root(w http.ResponseWriter, r *http.Request) {
+
 	c := appengine.NewContext(r)
 	u := user.Current(c)
+
 	if u == nil {
 		url, err := user.LoginURL(c, r.URL.String())
 		if err != nil {
@@ -25,5 +28,35 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusFound)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %v!", u)
+
+	fmt.Fprint(w, guestbookForm)
 }
+
+const guestbookForm = `
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+`
+
+func sign(w http.ResponseWriter, r *http.Request) {
+	err := signTemplate.Execute(w, r.FormValue("content"))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var signTemplate = template.Must(template.New("sign").Parse(signTemplateHTML))
+
+const signTemplateHTML = `
+<html>
+  <body>
+    <p>You wrote:</p>
+    <pre>{{.}}</pre>
+  </body>
+</html>
+`
